8b5fa7cf7bddc7e68f0147d4a39c72a57816b63a
Ali Jan <alijandeveloper15@gmail.com>
2025-03-05 02:13:38 -0800

    script updated

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ async function getWeather() {
         const data = await response.json();
         document.getElementById('weather-info').innerHTML = `
             <h2>${data.name}, ${data.sys.country}</h2>
-            <p>Temperature: ${data.main.temp}°C</p>
-            <p>Humidity: ${data.main.humidity}%</p>
-            <p>Wind Speed: ${data.wind.speed} m/s</p>
+            <p><i class="fas fa-thermometer-half"></i> Temperature: ${data.main.temp}°C</p>
+            <p><i class="fas fa-tint"></i> Humidity: ${data.main.humidity}%</p>
+            <p><i class="fas fa-wind"></i> Wind Speed: ${data.wind.speed} m/s</p>
         `;
     } catch (error) {
         document.getElementById('weather-info').innerHTML = '<p>City not found</p>';
